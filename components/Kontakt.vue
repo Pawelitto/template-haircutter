21ce2e1af600b23aae917465501052f9799238a1
Paweł Pietras <pawel.pietras144@gmail.com>
2024-03-13 09:46:08 +0100

    next klient

--- components/Kontakt.vue
+++ components/Kontakt.vue
@@ -37,9 +37,9 @@
         Chętnie odpowiemy na Twoje pytania i zapytania telefonicznie.
       </p>
       <NuxtLink
-        to="tel:+48605898932"
+        to="tel:+48513566650"
         class="font-semibold text-primary-600 dark:text-primary-500"
-        >+48 605 898 932</NuxtLink
+        >+48 513 566 650</NuxtLink
       >
     </div>
     <div>
@@ -57,7 +57,7 @@
         Sprawdź naszą lokalizację i odwiedź nas w dogodnym dla Ciebie terminie.
       </p>
       <NuxtLink
-        to="https://www.google.com/maps/dir/50.1555787,18.6642136/Fryzjer+Sabina+Baron,+Zwyci%C4%99stwa+16,+43-178+Ornontowice/@50.1589583,18.6313381,12z/data=!3m1!4b1!4m18!1m7!3m6!1s0x4711353c44b9fb09:0xf496449b6f1540c0!2sFryzjer+Sabina+Baron!8m2!3d50.1810319!4d18.7642638!16s%2Fg%2F11fzfdsg6c!4m9!1m1!4e1!1m5!1m1!1s0x4711353c44b9fb09:0xf496449b6f1540c0!2m2!1d18.764198!2d50.1810087!3e0?entry=ttu"
+        to="https://maps.app.goo.gl/oWcvB1mdmTs19SAb9"
         target="_blank"
         class="inline-flex py-2 px-4 text-sm font-medium text-center rounded-lg border text-primary-600 border-primary-600 hover:text-white hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:border-primary-500 dark:text-primary-500 dark:hover:text-white dark:hover:bg-primary-600 dark:focus:ring-primary-800"
         >Zobacz na mapie</NuxtLink
